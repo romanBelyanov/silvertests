4b28cbc5cbace670736004d1ca90bda95a111b8c
romanBelyanov <romkabel412@gmail.com>
2025-03-20 10:47:17 +0700

    Update 1.cpp

--- codeforces/1.cpp
+++ codeforces/1.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    int n, a, b, c;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> a >> b >> c;
+        if(a < b && b < c){
+            cout << "STAIR" << endl;
+        } else if(a < b && b > c){
+            cout << "PEAK" << endl;
+        } else {
+            cout << "NONE" << endl;
+        }
+    }
+}
